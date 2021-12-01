28752eb80cdd37b941e5f49fa1ed1f2b63880f7d
JonathasLeite <jonathas.leite@yahoo.com.br>
2021-12-01 06:03:30 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using POO;
+
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno p1 = new Aluno();
+
+            Console.WriteLine("Nome do Aluno");
+            p1.nome = Console.ReadLine();
+            Console.WriteLine("Nota");
+            p1.Nota = int.Parse(Console.ReadLine());
+
+
+        //    //valores validos
+        //    Retangulo r = new Retangulo();
+        //    r.DefinirMedidas(30, 30);
+        //    Console.WriteLine($"àrea: {r.ObterArea()}");
+
+        //    //valores Invalidos
+        //    Retangulo r2 = new Retangulo();
+        //    r.DefinirMedidas(0, 0);
+        //    Console.WriteLine($"àrea: {r2.ObterArea()}");
+        //
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POO;
+
+namespace POO
+{
+    class Retangulo
+    {
+        private double comprimento;
+        private double largura;
+        private bool valido;
+
+        public void DefinirMedidas(double comprimento, double largura)
+        {
+            if (comprimento < 0 && largura < 0)
+            {
+                this.comprimento = comprimento;
+                this.largura = largura;
+                valido = true;
+            }
+            else
+            {
+                Console.WriteLine("Valores Invalidos");
+            }
+        }
+
+        public double ObterArea()
+        {
+            if (valido) { 
+            return comprimento * largura;
+            }
+            else
+            {
+                Console.WriteLine("Preencha Valores validos");
+                return 0;
+            }
+        }
+    }
+}
